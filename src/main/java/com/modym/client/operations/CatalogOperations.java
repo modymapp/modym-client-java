7c2d02fd1bad16bd045cb389107b9683b4052c43
Bashar Saleh <bashar@imenaloyalty.com>
2016-06-07 11:03:00 +0400

    Updating to v0.1.1-alpha and adding new catalog methods specifically around product search and sort.

--- src/main/java/com/modym/client/operations/CatalogOperations.java
+++ src/main/java/com/modym/client/operations/CatalogOperations.java
@@ -3,7 +3,10 @@
  */
 package com.modym.client.operations;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.modym.client.ModymClientException;
 import com.modym.client.objects.ModymCategory;
@@ -41,7 +44,28 @@ public class CatalogOperations extends AbstractOperations {
      */
     public PageResponse<ModymCategory> getCategories() throws ModymClientException {
         String path = "catalog/categories";
-        return this.transport.doGet(path, null, null, CategoryPageResponse.class).getResult();
+        Map<String, Object> params = new HashMap<>();
+        params.put("page", 0);
+        params.put("size", 10);
+        return this.transport.doGet(path, params, null, CategoryPageResponse.class).getResult();
+    }
+
+    /**
+     * @param categoryId
+     * @return
+     * @throws ModymClientException
+     */
+    public PageResponse<ModymCategory> getCategories(int page, int size) throws ModymClientException {
+        if (page < 0)
+            throw new ModymClientException("Page number cannot be less than 0");
+        if (size < 1)
+            throw new ModymClientException("Page size cannot be less than 1");
+
+        String path = "catalog/categories";
+        Map<String, Object> params = new HashMap<>();
+        params.put("page", page);
+        params.put("size", size);
+        return this.transport.doGet(path, params, null, CategoryPageResponse.class).getResult();
     }
 
     /**
@@ -71,9 +95,65 @@ public class CatalogOperations extends AbstractOperations {
      * @return
      * @throws ModymClientException
      */
-    public PageResponse<ModymProduct> getCategoryProducts(long categoryId, int page, int size) throws ModymClientException {
+    public PageResponse<ModymProduct> getProducts(
+            int page,
+            int size,
+            String sort,
+            boolean ascending,
+            boolean includeInactive) throws ModymClientException {
         String path = "catalog/products/search";
-        Map<String, Object> params = ModymMapUtils.asMap("method", "category", "id", categoryId, "page", page, "size", size);
+        Map<String, Object> params = new HashMap<>();
+        params.put("method", "all");
+        params.put("id", "0");
+        params.put("page", page);
+        params.put("size", size);
+        params.put("includeInActive", includeInactive);
+        if (StringUtils.isNotBlank(sort)) {
+            params.put("sort", sort);
+            params.put("direction", ascending ? "ASC" : "DESC");
+        }
+        return this.transport.doGet(path, params, null, ProductPageResponse.class).getResult();
+    }
+
+    /**
+     * @param categoryId
+     * @param page
+     * @param size
+     * @return
+     * @throws ModymClientException
+     */
+    public PageResponse<ModymProduct> getFeaturedProducts(
+            int page,
+            int size,
+            String sort,
+            boolean ascending,
+            boolean includeInactive) throws ModymClientException {
+        String path = "catalog/products/search";
+        Map<String, Object> params = new HashMap<>();
+        params.put("method", "featured");
+        params.put("id", "0");
+        params.put("page", page);
+        params.put("size", size);
+        params.put("includeInActive", includeInactive);
+        if (StringUtils.isNotBlank(sort)) {
+            params.put("sort", sort);
+            params.put("direction", ascending ? "ASC" : "DESC");
+        }
+        return this.transport.doGet(path, params, null, ProductPageResponse.class).getResult();
+    }
+
+    /**
+     * @param categoryId
+     * @param page
+     * @param size
+     * @return
+     * @throws ModymClientException
+     */
+    public PageResponse<ModymProduct> getCategoryProducts(long categoryId, int page, int size)
+            throws ModymClientException {
+        String path = "catalog/products/search";
+        Map<String, Object> params =
+                ModymMapUtils.asMap("method", "category", "id", categoryId, "page", page, "size", size);
         return this.transport.doGet(path, params, null, ProductPageResponse.class).getResult();
     }
 
